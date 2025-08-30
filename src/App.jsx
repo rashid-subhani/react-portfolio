dedd72f382a56547dd3dcff8bea1ec07db3ebca1
rashidsubhani <ras_sub2003@yahoo.com>
2025-08-31 00:48:45 +0100

    routes and page layout created

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,32 @@
-// src/App.jsx
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import About from './pages/About';
-import Projects from './pages/Projects';
-import Contact from './pages/Contact';
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Projects from "./pages/Projects.jsx";
+import Contact from "./pages/Contact.jsx";
+import Footer from "./components/Footer.jsx";
 
-function App() {
+export default function App(){
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </Router>
+    <>
+      <header className="nav">
+        <div className="container">
+          <Navbar/>
+        </div>
+      </header>
+
+      <main className="container">
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/projects" element={<Projects/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+        </Routes>
+      </main>
+
+      <footer className="container">
+        <Footer/>
+      </footer>
+    </>
   );
 }
-
-export default App;
